82dde2eaeb0ab0262fbc98a467d5cd56768f723e
nickfree4437x <146987759+nickfree4437x@users.noreply.github.com>
2025-07-23 13:06:56 +0530

    Update Login.jsx

--- frontend/src/Components/Admin/Auth/Login.jsx
+++ frontend/src/Components/Admin/Auth/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", formData);
+      const res = await axios.post("https://attendmaster.onrender.com/api/auth/login", formData);
       localStorage.setItem("token", res.data.token);
       
       Swal.fire({
