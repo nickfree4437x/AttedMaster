d7f96c84794c53d282a95b3c4aadeb97ba6a40b9
nickfree4437x <146987759+nickfree4437x@users.noreply.github.com>
2025-07-23 13:20:27 +0530

    Update AttendanceState.jsx

--- frontend/src/Components/Student/AttendanceRecords/AttendanceState.jsx
+++ frontend/src/Components/Student/AttendanceRecords/AttendanceState.jsx
@@ -30,7 +30,7 @@ function AttendanceState() {
     const fetchAttendance = async () => {
       try {
         const token = localStorage.getItem('studentToken');
-        const res = await axios.get('http://localhost:5000/api/students/my-attendance', {
+        const res = await axios.get('https://attendmaster.onrender.com/api/students/my-attendance', {
           headers: { Authorization: `Bearer ${token}` },
         });
 
